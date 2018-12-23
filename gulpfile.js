27f3584ad86e90d930a3795ffba4c9e38aa958b7
syagawa <yagawa@qzc.co.jp>
2018-12-23 21:03:38 +0900

    Update package

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const html2pug = require('gulp-html2pug');
 gulp.task('pug', function() {
   // Backend locales
   return gulp.src('convert.html')
-  .pipe(html2pug(/* options for html2pug such as { fragment: true } */))
+  .pipe(html2pug({ fragment: true }))
   .pipe(gulp.dest('pug'));
 });
 

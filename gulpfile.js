f9d9190d5daeecad41ebd4a005df4427fd19a3cb
yagawa <yagawa@qzc.co.jp>
2020-01-15 12:33:02 +0900

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 const gulp = require('gulp');
 const html2pug = require('gulp-html2pug');
 
+const target = ["./convert.html"];
+
 function h2pug() {
-  return gulp.src('convert.html')
+  return gulp.src(target)
   .pipe(html2pug({ fragment: true }))
   .pipe(gulp.dest('pug'));
 }
 
 function watch(){
-  gulp.watch(["./convert.html"], h2pug);
+  gulp.watch(target, h2pug);
 }
 
 const start = gulp.series(h2pug, watch);
